c37ee7bf6c8bd22fab7d50276e0b3439e87f6e68
ai_nick <alittlejoke@mail.ru>
2023-01-28 11:02:35 +0300

    ex 33 is not working

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 33: Задайте массив. Напишите программу, которая 
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет -3; массив [6, 7, 19, 345, 3] -> да */
+
+int[] array = new int[3]{1,2,3};
+bool result = Contains(array, 3);
+Console.WriteLine(result);
+bool Contains(int[] array, int number) 
+{
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
